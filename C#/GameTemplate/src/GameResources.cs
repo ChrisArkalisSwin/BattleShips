c400ababbf6fc7f10b2ffdc44c453d6c2dc18fc2
Mitchwinsbig <102117313@student.swin.edu.au>
2019-05-08 15:55:13 +1000

    New background music

--- C#/GameTemplate/src/GameResources.cs
+++ C#/GameTemplate/src/GameResources.cs
@@ -59,7 +59,7 @@ public static class GameResources
 
 	private static void LoadMusic()
 	{
-		NewMusic("Background", "horrordrone.mp3");
+		NewMusic("Background", "NyanCat.mp3");
 	}
 
 	/// <summary>
